22909c95b6ce6166fdf49e601e8415fe0952b7a5
sadooghi <gh.sadooghi@gmail.com>
2017-02-17 11:30:22 -0600

    add loopyLighthouse v1&2

---
+++ loopyLighthouse1.js
@@ -0,0 +1,16 @@
+var i = 100;
+while (i >= 100 && i <= 200) {
+  if ( i % 3 === 0 && i % 4 === 0) {
+    console.log("LoopyLighthouse")
+  }
+  else if ( i % 3 === 0) {
+    console.log("Loopy");
+  }
+  else if ( i % 4 === 0) {
+    console.log("Lighthouse")  
+  } 
+  else {
+    console.log(i);
+  }
+  i ++;
+}
